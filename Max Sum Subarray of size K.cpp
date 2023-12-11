f1dbd7b5d7dc65b36db1249f801086880bd303ea
Jai Bairagi <110719651+jaibairagi01@users.noreply.github.com>
2023-12-11 12:32:41 +0530

    Create Max Sum Subarray of size K.cpp

---
+++ Max Sum Subarray of size K.cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:
+    long maximumSumSubarray(int K, vector<int> &Arr, int N){
+
+    long ans = 0, sum = 0;
+    for(int i = 0; i < N; i++){
+        if(i<K){
+           sum += Arr[i];
+        }
+         else{
+              ans = max(ans,sum);
+              sum += Arr[i];
+              sum -= Arr[i-K];
+         }
+    }
+    ans = max(ans,sum);
+    return ans;
+    }
+}

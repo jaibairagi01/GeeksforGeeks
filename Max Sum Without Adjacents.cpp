10a1c2e7d960fecd3bbe3b8392b8d836a459e5ba
Jai Bairagi <110719651+jaibairagi01@users.noreply.github.com>
2023-12-17 07:55:19 +0530

    Create Max Sum Without Adjacents.cpp

---
+++ Max Sum Without Adjacents.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:	
+	// calculate the maximum sum with out adjacent
+	int findMaxSum(int *arr, int n) {
+	    // code here
+	    int ans=0, mx=0;
+	    for(int i=0; i<n; i++){
+	        int val = arr[i]+mx;
+	        mx = max(mx,ans);
+	        ans = max(ans,val);
+	    }
+	    return ans;
+	}
+};

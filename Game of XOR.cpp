13ed522dd0b88d60c2b8d8cdb55732653804c280
Jai Bairagi <110719651+jaibairagi01@users.noreply.github.com>
2023-12-18 11:28:56 +0530

    Create Game of XOR.cpp

---
+++ Game of XOR.cpp
@@ -0,0 +1,12 @@
+class Solution{
+  public:
+     int gameOfXor(int N , int A[]) {
+        // code here
+        if(N%2==0) return 0;
+        int ans=A[0];
+        for(int i=2; i<N; i+=2){
+            ans^=A[i];
+        }
+        return ans;
+    }
+};

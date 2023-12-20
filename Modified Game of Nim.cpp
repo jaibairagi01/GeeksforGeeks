b0c771c633065a74e48bbe30b8443ba164e85f3d
Jai Bairagi <110719651+jaibairagi01@users.noreply.github.com>
2023-12-20 10:34:01 +0530

    Create Modified Game of Nim.cpp

---
+++ Modified Game of Nim.cpp
@@ -0,0 +1,11 @@
+class Solution{
+public:
+    int findWinner(int n, int A[]){
+        // code here
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            ans ^= A[i];
+        }
+        return ans == 0 or n%2 == 0 ? 1 : 2;
+    }
+};

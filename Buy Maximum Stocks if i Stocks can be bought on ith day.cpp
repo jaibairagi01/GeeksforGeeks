514fc13a70b0cbb2045a1c02277cfcfaf99262c0
Jai Bairagi <110719651+jaibairagi01@users.noreply.github.com>
2023-12-24 18:56:51 +0530

    Create Buy Maximum Stocks if i Stocks can be bought on ith day.cpp

---
+++ Buy Maximum Stocks if i Stocks can be bought on ith day.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int buyMaximumProducts(int n, int k, int price[]){
+        //Write your code here
+        vector<pair<int, int>> v;
+        for(int i=0; i<n; i++)
+         v.push_back({price[i], i+1});
+         
+        sort(v.begin(), v.end());
+        
+        int ans = 0;
+        
+        for(auto it:v){
+            int maxBuy = min(k / it.first, it.second);
+            ans += maxBuy;
+            k -= it.first *maxBuy;
+        }
+        return ans;
+    }
+};

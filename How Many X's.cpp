de3a957dfb6120e67f8c1b44074e5e994a3c5f22
Jai Bairagi <110719651+jaibairagi01@users.noreply.github.com>
2024-01-04 10:31:01 +0530

    Update How Many X's.cpp

--- How Many X's.cpp
+++ How Many X's.cpp
@@ -3,19 +3,15 @@ class Solution {
     int countX(int L, int R, int X) {
         // code here
         int cnt = 0;
-        for(int i = L+1; i < R; ++i){
-            cnt += countOccurence(i, X);
-        }
-        return cnt;
-    }
-    private:
-    int countOccurence(int num, int X){
-        int cnt = 0;
-        while(num > 0){
-            if(num % 10 == X){
-                ++cnt;
+        for(int i = L+1; i < R; i++){
+            int temp = i;
+            while(temp > 0){
+                int digit =  temp % 10;
+                if(digit == X){
+                    cnt++;
+                }
+                temp /= 10;
             }
-            num /= 10;
         }
         return cnt;
     }
